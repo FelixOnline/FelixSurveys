fbb6d520c72663aaa13abddebf971ca1fce32a53
Philip Kent <kentphilip@gmail.com>
2012-01-25 17:44:00 +0000

    Add transition for the modal dialog

--- index.php
+++ index.php
@@ -185,6 +185,7 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
 
+    <script src="js/bootstrap-transition.js"></script>
     <script src="js/bootstrap-modal.js"></script>
     <script src="js/bootstrap-tooltip.js"></script>
     <script src="js/bootstrap-popover.js"></script>
