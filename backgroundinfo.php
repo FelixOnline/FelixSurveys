899e8ff4ea72d6f346082d021bf67a3ca11b25dc
Philip Kent <kentphilip@gmail.com>
2012-01-26 23:13:13 +0000

    More question updates, also update count of number of questions available

--- backgroundinfo.php
+++ backgroundinfo.php
@@ -1,4 +1,4 @@
 <h3>Background information</h3>
 <p>If you don’t want to answer any question, just leave it at the default setting. At the end of the survey you will be asked confirm the questions you’ve left blank (to make sure they’re blank intentionally and not just overlooked).</p>
-<p>This questionnaire should take no more that 10 minutes. The number of questions varies depending on previous answers but the maximum number is 30.</p>
+<p>This questionnaire should take no more that 10 minutes. The number of questions varies depending on previous answers but the maximum number is 33.</p>
 <p>While this is a light-hearted exercise, honesty is encouraged. We would like reiterate again that your results are completely anonymous. Any concerns may be addressed to <a href="mailto: felix@imperial.ac.uk">felix@imperial.ac.uk</a>.</p>
