720a304c72227c6a8c745b288816b3e6478f35e6
Philip Kent <kentphilip@gmail.com>
2012-02-09 17:50:11 +0000

    Display human readable questions and answers Store troll check data in CSV export

--- dashboard/output.php
+++ dashboard/output.php
@@ -10,11 +10,15 @@ $fp = fopen('data.csv', 'w+');
 $questions = file_get_contents('../questions.json');
 $questions = json_decode($questions, true);
 
-foreach($questions as $question) {
+foreach($questions as $question) {	
     if(array_key_exists('name', $question)) {
-        $headers[] = $question['name'];
+    	preg_match('/(?<= - )(.*)((?=\?))/', $question['label'], $matches);
+        $headers[] = strip_tags($matches[0]);
+		$raw_headers[] = $question['name'];
     }
 }
+
+$headers[] = 'Troll check';
 
 fputcsv($fp, $headers);
 
@@ -25,11 +29,20 @@ while($row = mysql_fetch_array($rsc)) {
     $answers = array();
     $places = array();
     $data = json_decode($row['data'], true);
-    foreach($headers as $column) {
+    foreach($raw_headers as $column) {
         if(array_key_exists($column, $data)) {
             switch($column) {
                 case 'campuswhere':
-                    $answers[] = json_encode($data[$column]);
+					foreach ($questions['campuswhere']['options'] as $tmp_option) {
+						$tmp_label[] = $tmp_option['label'];
+						$tmp_value[] = $tmp_option['value'];
+					}
+					
+					foreach ($data[$column] as $tmp_choice) {
+						$final_data[] = str_replace($tmp_value, $tmp_label, $tmp_choice);
+					}
+					
+                    $answers[] = json_encode($final_data);
                     break;
                 case 'other':
                     $order   = array("\r\n", "\n", "\r");
@@ -37,13 +50,25 @@ while($row = mysql_fetch_array($rsc)) {
                     $answers[] = str_replace($order, $replace, $data[$column]);
                     break;
                 default:
-                    $answers[] = $data[$column];
+					$realanswers = $questions[$column]['options'];
+					foreach ($realanswers as $option) {
+						if($option['value'] == $data[$column]) {
+							$answers[] = $option['label'];
+						}
+					}
                     break;
             }
         } else {
             $answers[] = 'NULL';
         }
     }
+	
+	if($row['deptcheck'] == 1) {
+		$answers[] = 'Possible troll';
+	} else {
+		$answers[] = 'Not flagged';
+	}
+	
     fputcsv($fp, $answers);
 }
 
