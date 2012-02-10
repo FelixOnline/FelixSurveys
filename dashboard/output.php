5feeffb0b10a1ac449d147e7b966a82ac135725c
Philip Kent <kentphilip@gmail.com>
2012-02-10 22:31:08 +0000

    Fix a bug with questions with no question mark at the end If a question was not shown due to dependencies, set it's value to N/A

--- dashboard/output.php
+++ dashboard/output.php
@@ -10,9 +10,9 @@ $fp = fopen('data.csv', 'w+');
 $questions = file_get_contents('../questions.json');
 $questions = json_decode($questions, true);
 
-foreach($questions as $question) {	
+foreach($questions as $question) {
     if(array_key_exists('name', $question)) {
-    	preg_match('/(?<= - )(.*)((?=\?))/', $question['label'], $matches);
+    	preg_match('/(?<= - )(.*)$/', $question['label'], $matches);
         $headers[] = strip_tags($matches[0]);
 		$raw_headers[] = $question['name'];
     }
@@ -29,35 +29,66 @@ while($row = mysql_fetch_array($rsc)) {
     $answers = array();
     $places = array();
     $data = json_decode($row['data'], true);
+
     foreach($raw_headers as $column) {
         if(array_key_exists($column, $data)) {
-            switch($column) {
-                case 'campuswhere':
-					foreach ($questions['campuswhere']['options'] as $tmp_option) {
-						$tmp_label[] = $tmp_option['label'];
-						$tmp_value[] = $tmp_option['value'];
-					}
-					
-					foreach ($data[$column] as $tmp_choice) {
-						$final_data[] = str_replace($tmp_value, $tmp_label, $tmp_choice);
-					}
-					
-                    $answers[] = json_encode($final_data);
-                    break;
-                case 'other':
-                    $order   = array("\r\n", "\n", "\r");
-                    $replace = '<br />';
-                    $answers[] = str_replace($order, $replace, $data[$column]);
-                    break;
-                default:
-					$realanswers = $questions[$column]['options'];
-					foreach ($realanswers as $option) {
-						if($option['value'] == $data[$column]) {
-							$answers[] = $option['label'];
+        	$dependent = false;
+        	if(array_key_exists('dependencies', $questions[$column])) {
+        		$dependent = true;
+        		$required = true;
+				$passed = true; // Assume we met the dependency
+				
+				if(array_key_exists('reverse', $questions[$column]) && $questions[$column]['reverse'] == true) {
+					$required = false;
+					$passed = true; // Assume the dependency is not met. This means that this test passed
+				}
+				
+				foreach($questions[$column]['dependencies'] as $dependency) {
+					if($required) {
+						if ($data[$dependency['id']] != $dependency['value']) {
+							// If we did not meet the dependency, mark as so
+							$passed = false;
+						}
+					} else {
+						if ($data[$dependency['id']] == $dependency['value']) {
+							// If we met the dependency, then in reverse mode the test has not passed
+							$passed = false;
 						}
 					}
-                    break;
-            }
+				}
+        	}
+			
+			if($dependent == false || ($dependent == true && $passed == true)) {
+	            switch($column) {
+	                case 'campuswhere':
+						foreach ($questions['campuswhere']['options'] as $tmp_option) {
+							$tmp_label[] = $tmp_option['label'];
+							$tmp_value[] = $tmp_option['value'];
+						}
+						
+						foreach ($data[$column] as $tmp_choice) {
+							$final_data[] = str_replace($tmp_value, $tmp_label, $tmp_choice);
+						}
+						
+	                    $answers[] = json_encode($final_data);
+	                    break;
+	                case 'other':
+	                    $order   = array("\r\n", "\n", "\r");
+	                    $replace = '<br />';
+	                    $answers[] = str_replace($order, $replace, $data[$column]);
+	                    break;
+	                default:
+						$realanswers = $questions[$column]['options'];
+						foreach ($realanswers as $option) {
+							if($option['value'] == $data[$column]) {
+								$answers[] = $option['label'];
+							}
+						}
+	                    break;
+	            }
+			} else {
+				$answers[] = 'N/A';
+			}
         } else {
             $answers[] = 'NULL';
         }
@@ -68,7 +99,6 @@ while($row = mysql_fetch_array($rsc)) {
 	} else {
 		$answers[] = 'Not flagged';
 	}
-	
     fputcsv($fp, $answers);
 }
 
