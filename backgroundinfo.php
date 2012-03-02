157e9d227e09bf3651cd01f23c14189d13c6a7de
Philip Kent <kentphilip@gmail.com>
2012-03-02 18:00:23 +0000

    Update documents

--- backgroundinfo.php
+++ backgroundinfo.php
@@ -1,2 +1,4 @@
 <h3>Background information</h3>
-<p>more blah</p>
+<p>If you don’t want to answer any question, just leave it at the default setting. At the end of the survey you will be asked confirm your answers.</p>
+<p>This questionnaire should take no more than 10 minutes. In total, there are 15 questions to answer.</p>
+<p>Honesty is encouraged. We would like to reiterate again that your results are completely anonymous. Any concerns may be addressed to <a href="mailto:felix@imperial.ac.uk">felix@imperial.ac.uk</a></p>
--- introduction.php
+++ introduction.php
@@ -1,4 +1,4 @@
 <h3>Introduction</h3>
-<p>Blah blah blah privacy details here</p>
+<p>Thank you for agreeing to take part in the Felix Sexism Survey. Sexism has proved to be a real debate this year at Imperial College; we would like to hear your opinion and feelings on this. The results are completely confidential. For complete information on the privacy and data protection, click the button below. In brief, your username is only required to prevent trolling and restrict the questionnaire to Imperial students. At no point will it be possible for your answer to be matched to you.</p>
 <p><a data-toggle="modal" href="#more-details" >More information</a></p>
 <?php require ('privacybox.php'); ?>
