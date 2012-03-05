3c4f172edb0e5c5877512297ffdfc9155ffabec2
Philip Kent <kentphilip@gmail.com>
2012-03-05 16:02:35 +0000

    Fix output script and generalise

--- dashboard/output.php
+++ dashboard/output.php
@@ -62,20 +62,8 @@ while($row = mysql_fetch_array($rsc)) {
         	}
 			
 			if($dependent == false || ($dependent == true && $passed == true)) {
-	            switch($column) {
-	                case 'campuswhere':
-						foreach ($questions['campuswhere']['options'] as $tmp_option) {
-							$tmp_label[] = $tmp_option['label'];
-							$tmp_value[] = $tmp_option['value'];
-						}
-						
-						foreach ($data[$column] as $tmp_choice) {
-							$final_data[] = str_replace($tmp_value, $tmp_label, $tmp_choice);
-						}
-						
-	                    $answers[] = json_encode($final_data);
-	                    break;
-	                case 'other':
+	            switch($questions[$column]['type']) {
+	                case 'textbox':
 	                    $order   = array("\r\n", "\n", "\r");
 	                    $replace = '<br />';
 	                    $answers[] = str_replace($order, $replace, $data[$column]);
@@ -107,51 +95,9 @@ while($row = mysql_fetch_array($rsc)) {
 
 fclose($fp);
 
-unset($headers);
-
-// RESPONSES FOR CAMPUSWHERE
-$fp = fopen('data_locations.csv', 'w+');
-
-$i = 0;
-$headers = array();
-$headers_raw = array();
-foreach($questions['campuswhere']['options'] as $location) {
-    $headers[$i] = $location['label'];
-	$headers_raw[$i] = $location['value'];
-	$i++;
-}
-
-fputcsv($fp, $headers);
-
-$sql = "SELECT * FROM `sexsurvey_responses`";
-$rsc = mysql_query($sql);
-
-while($row = mysql_fetch_array($rsc)) {
-    $answers = array();
-    $places = array();
-    $data = json_decode($row['data'], true);
-	
-	$this_row = array();
-	
-	if(array_key_exists('campuswhere', $data)) {
-		foreach($headers_raw as $id => $location) {
-			if(array_search($location, $data['campuswhere']) !== false) {
-				$this_row[$id] = 1;
-			} else {
-				$this_row[$id] = 0;
-			}
-		}
-		
-		fputcsv($fp, $this_row);
-	}
-}
-
-fclose($fp);
-
 ?>
 
 <h1>Export complete</h1>
 <ul>
 	<li><b>data.csv</b> contains the data on every question, as well as the troll check bit</li>
-	<li><b>data_locations.csv</b> contains true/fale values for each location in the locations list, for every response specified. This is easier to analyse than the values for this question in data.csv</li>
 </ul>
